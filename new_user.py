af63500752a31751e9a9f1755ddd43f7665cc804
claspourhands <stefan.setyadi.96@gmail.com>
2016-10-14 18:04:25 +0800

    new user.py

---
+++ new_user.py
@@ -0,0 +1,9 @@
+with open("user_id trackcount.txt",'r') as myFile:
+    doc = myFile.read().split('\n')
+
+playcount = [i.partition("\t") for i in doc[1:]]
+print len(playcount)
+new_user = [i[0] for i in playcount]
+
+with open("new_user.txt",'w') as new_user_file:
+    new_user_file.write("\n".join(new_user))
