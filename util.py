2c326c6ee84848718703ff7c1b5b973199c4b37e
claspourhands <stefan.setyadi.96@gmail.com>
2016-10-15 20:22:25 +0800

    change script

--- util.py
+++ util.py
@@ -1,3 +1,7 @@
 def head(myList,start,end):
     for i in myList[start:end]:
-        print i
+        print repr(i)
+
+def writeToFile(myStr, filename):
+    with open(filename,'w') as newFile:
+        newFile.write(myStr)
