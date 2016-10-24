0ef1665b4d40c5d83843df024a321bab67d26d6d
Zillion-Govin <zillion.govin@gmail.com>
2016-10-24 12:09:42 +0800

    spring cleaning

--- Music dataset(in case)/new_user.py
+++
@@ -1,9 +0,0 @@
-with open("dataset(splitted)\\user_id trackcount.txt",'r') as myFile:
-    doc = myFile.read().split('\n')
-
-playcount = [i.partition("\t") for i in doc[1:]]
-print len(playcount)
-new_user = [i[0] for i in playcount if i[2] not in '12']
-
-with open("new_user.txt",'w+') as new_user_file:
-    new_user_file.write("\n".join(new_user))
--- util.py
+++
@@ -1,7 +0,0 @@
-def head(myList,start,end):
-    for i in myList[start:end]:
-        print repr(i)
-
-def writeToFile(myStr, filename):
-    with open(filename,'w') as newFile:
-        newFile.write(myStr)
