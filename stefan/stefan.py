f8eb7f8bae25c8248c0bdb26b6d2867583bc8856
claspourhands <stefan.setyadi.96@gmail.com>
2016-10-25 09:35:11 +0800

    correlation sim

--- stefan/stefan.py
+++ stefan/stefan.py
@@ -19,33 +19,27 @@ def predict_wsum(ratings,similarity):
     #print(np.array([np.abs(similarity).sum(axis=1)]))
     return ratings.dot(similarity)/ np.array([np.abs(similarity).sum(axis=1)])
     
-#change later to load from file
-def train_test_split(ratings):
-    test = np.zeros(ratings.shape)
-    train = ratings.copy()
-    for user in range(ratings.shape[0]):
-        test_ratings = np.random.choice(ratings[user, :].nonzero()[0], 
-                                        size=10, 
-                                        replace=False)
-        train[user, test_ratings] = 0.
-        test[user, test_ratings] = ratings[user, test_ratings]
-        
-    # Test and training are truly disjoint
-    assert(np.all((train * test) == 0)) 
-    return train, test    
-    
 def get_mse(pred, actual):
-    # Ignore nonzero terms.
     pred = pred[actual.nonzero()].flatten()
     actual = actual[actual.nonzero()].flatten()
     return mse(pred, actual)    
     
-pathName = "ratings.csv"
-names = ['user_id','item_id','rating','timestamp']
-df = pd.read_csv(pathName, sep=',', names=names)
-df.drop('timestamp',axis=1,inplace=True)
+def loadDF(pathName,name,cut_last=True):
+    temp_df = pd.read_csv(pathName, sep=',', names=name)
+    if(cut_last):
+        temp_df.drop(name[-1:], axis=1, inplace=True)    
+    return temp_df
+
+
+trainingPathName = "./../dataset_partitioned/ratings_training.csv"
+testPathName = "./../dataset_partitioned/ratings_test.csv"
+headerName = ['user_id','item_id','rating','timestamp']
+
+train_df = loadDF(trainingPathName,headerName)
+test_df = loadDF(testPathName, headerName)
     
-n_item = df.item_id.unique()
+n_item = train_df.item_id.unique()
+## create index between df index and raw item_id
 index2_id = {}
 id2_index = {}
 for i in range(n_item.shape[0]):
@@ -54,21 +48,27 @@ for i in range(n_item.shape[0]):
     id2_index[n_item[i]] = i
 
 n_item = n_item.shape[0]
-n_user = df.user_id.unique().shape[0]
+n_user = train_df.user_id.unique().shape[0]
 
 #print("{} {}".format(n_user,n_item))
 
-
-mat = np.zeros((n_user,n_item))
-for i in df.itertuples():
+zeroes = np.zeros((n_user,n_item))
+## fill up matrix with training data
+train = zeroes
+for i in train_df.itertuples():
     #print( id2_index[i[2]])    
-    mat[i[1]-1, id2_index[i[2]]] = i[3]
-print(mat[:4,:4])
+    train[i[1]-1, id2_index[i[2]]] = i[3]
+print(train[:4,:4])
 
-sparsity = float(len(mat.nonzero()[0])) / n_user / n_item * 100
+## create separate matrix to contain actual data (for testing)
+test=zeroes
+for i in test_df.itertuples():
+    #print( id2_index[i[2]])    
+    train[i[1]-1, id2_index[i[2]]] = i[3]
+
+sparsity = float(len(train.nonzero()[0])) / n_user / n_item * 100
 print("Sparsity: {:6.2f}%".format(sparsity))
 
-train,test = train_test_split(mat)
 
 item_sim = cos_sim(train)
 print(item_sim[:4,:4])
