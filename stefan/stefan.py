afb0ea606b7b3b5353a7b7fb57cb59f16ed83b58
claspourhands <stefan.setyadi.96@gmail.com>
2016-10-29 17:44:48 +0800

    top10

--- stefan/stefan.py
+++ stefan/stefan.py
@@ -43,8 +43,28 @@ def loadDF(pathName,name,cut_last=True):
 
 def get_topk_pos(similarity,k):
     ## argpartition will return topk but not ordered
-    return np.argpartition(-similarity,k)[:,1:k+1]
+    return np.argpartition(-similarity,(0,k))[:,1:k+1]
 
+def topk_mse(train,test,item_sim,k):
+    k_mse = []
+    starttime = time.time()
+    for i in k:
+        prediction = predict_topk(train,item_sim,i)
+        #print(prediction[:4,:4])
+        k_mse.append(get_mse(prediction,test))
+        print("{} {}".format(i,time.time()-starttime))
+        starttime=time.time()
+    print(k_mse)
+
+def topk_to_file(writePathName, item_sim,k):
+    writeStr = []
+    top10 = get_topk_pos(item_sim,k) # without the item itself
+    for i in range(len(top10)):
+        writeStr.append("{}:{}".format(index2_id[i],",".join([str(index2_id[j]) for j in top10[i]])))
+    writeStr = "\n".join(writeStr)
+    with open(writePathName, 'w') as writeFile:
+        writeFile.write(writeStr)
+        
 trainingPathName = "./../dataset_partitioned/ratings_training.csv"
 testPathName = "./../dataset_partitioned/ratings_test.csv"
 headerName = ['user_id','item_id','rating','timestamp']
@@ -66,37 +86,34 @@ n_user = train_df.user_id.unique().shape[0]
 
 #print("{} {}".format(n_user,n_item))
 
-zeroes = np.zeros((n_user,n_item)).astype(np.float32)
 ## fill up matrix with training data
-train = zeroes
+train = np.zeros((n_user,n_item)).astype(np.float32)
 for i in train_df.itertuples():
     #print( id2_index[i[2]])    
     train[i[1]-1, id2_index[i[2]]] = i[3]
 print(train[:4,:4])
 
 ## create separate matrix to contain actual data (for testing)
-test=zeroes
+test = np.zeros((n_user,n_item)).astype(np.float32)
 for i in test_df.itertuples():
     #print( id2_index[i[2]])    
-    train[i[1]-1, id2_index[i[2]]] = i[3]
+    test[i[1]-1, id2_index[i[2]]] = i[3]
 
 sparsity = float(len(train.nonzero()[0])) / n_user / n_item * 100
-print("Sparsity: {:6.2f}%".format(sparsity))
+print("Train Sparsity: {:6.2f}%".format(sparsity))
+sparsity = float(len(test.nonzero()[0])) / n_user / n_item * 100
+print("Test Sparsity: {:6.2f}%".format(sparsity))
 
 
 item_sim = cos_sim(train)
 #print(item_sim[:4,:4])
 
 print(test[:4,:4])
-#prediction = predict_wsum(train,item_sim)
-#k = [1,2,5,10,20,40,100,200,500,1000]
-k = [3,4,5,6,7]
-k_mse = []
-starttime = time.time()
-for i in k:
-    prediction = predict_topk(train,item_sim,i)
-    #print(prediction[:4,:4])
-    k_mse.append(get_mse(prediction,test))
-    print("{} {}".format(i,time.time()-starttime))
-    starttime=time.time()
-print(k_mse)
+
+k = [1,2,5,10,20,40,100,200]#,500]
+#k = [3,4,5,6,7]
+
+topk_mse(train,test,item_sim,k)
+
+writePath = "top10.txt"
+topk_to_file(writePath,item_sim,10)
