3008988a7f79b4d1363faf5603b9e63bf746b178
Stefan0010 <dphoenix32@yahoo.com>
2016-11-13 02:38:00 +0900

    add comment

--- src/matrix.py
+++ src/matrix.py
@@ -5,10 +5,12 @@ import os
 from sklearn.cross_validation import train_test_split
 
 ENCODING = 'ASCII'
-FILE_PATH = 'dataset_matrix\\matrix.csv'
+FILE_PATH_TRAIN = 'dataset_matrix\\matrix.csv'
+FILE_PATH_TEST = 'dataset_matrix\\matrix_test.csv'
 TRAIN_SOURCE = 'dataset_partitioned\\ratings_training.dat'
 TEST_SOURCE = 'dataset_partitioned\\ratings_test.dat'
 
+# generate the list for movie and user from the source to be used as a column and row for the matrix
 def get_list(path):
     movie_list ={}
     user_list = {}
@@ -19,6 +21,7 @@ def get_list(path):
             user_list[line[0]] = 0
     return movie_list,user_list
 
+# create the file for the matrix, if its not exist then it will create the file first
 def create_file(data,path):
     if not os.path.exists(path):
         with io.open(path,'w+',encoding = ENCODING):
@@ -26,8 +29,10 @@ def create_file(data,path):
     data.to_csv(path, encoding=ENCODING)
     print 'finish'
 
+# create the matrix based on the source list
 def create_matrix(path,dest):
     movie_list,user_list = get_list(path)
+    # create the dataframe based on the lists provided with uint8 data type to save the size of the matrix
     data = pandas.DataFrame(0,columns=user_list.keys(),index=movie_list.keys(),dtype='uint8')
     with io.open(path, 'r', encoding=ENCODING) as source:
         for i, lines in enumerate(source, 0):
@@ -36,9 +41,6 @@ def create_matrix(path,dest):
     data.replace(0, numpy.NaN, inplace=True)
     create_file( data= data,path=dest)
 
-def get_matrix(path):
-    data = pandas.DataFrame.from_csv(path,encoding = ENCODING)
-    return data
-
+# change the path and dest to test path if wanted to create test matrix
 if __name__ == "__main__":
-    create_matrix(path = TRAIN_SOURCE,dest = FILE_PATH)
+    create_matrix(path = TRAIN_SOURCE,dest = FILE_PATH_TRAIN)
